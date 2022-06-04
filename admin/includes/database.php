7e27c66491e38ad98ce5278fe867742dba7e29df
Nikita <kent720p@gmail.com>
2022-06-04 03:43:59 +0200

    added query method

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -2,16 +2,31 @@
 require_once("new_config.php");
 
 class Database {
-    private $conn;
+
+
+    public $conn;
+
+    function __construct()
+    {
+        $this->open_db_connection();
+    }
     public function open_db_connection() {
         $this->conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         if(mysqli_connect_errno()) {
             die("DATABASE failed " . mysqli_error($this->conn));
         }
     }
+
+    public function query($sql) {
+        $result = mysqli_query($this->conn, $sql);
+        if(!$result) {
+            die("Query failed");
+        }
+        return $result;
+    }
 }
 
-
+$db = new Database();
 
 
 
