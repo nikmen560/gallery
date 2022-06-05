011f4cde975109bfbda627e4ee78210910621bcd
Nikita <kent720p@gmail.com>
2022-06-05 04:09:18 +0200

    created functions.php file, added autoload function

---
+++ admin/includes/functions.php
@@ -0,0 +1,12 @@
+<?php 
+spl_autoload_register(function ($class) {
+    $class = strtolower($class);
+    $path = "{$class}.php";
+    if(file_exists($path)) {
+        require_once($path);
+    } else {
+        die("file '{$class}.php' was not found");
+    }
+})
+
+?>
