8aea581991e4f16044f5f49543f5b478bb96a4e9
Nikita <kent720p@gmail.com>
2022-06-12 04:13:58 +0200

    abstracted method create user

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,7 +2,8 @@
 
 class User
 {
-
+    protected static $db_table = "users";
+    protected static $db_table_fields = array('username', 'user_password', 'user_first_name', 'user_last_name');
     public $user_id;
     public $username;
     public $user_password;
@@ -63,42 +64,88 @@ class User
         return isset($this->user_id) ? $this->update() : $this->create();
     }
 
+    protected function properties()
+    {
+        // return get_object_vars($this);
+        $properties = array();
+        foreach (self::$db_table_fields as $db_field) {
+            if (property_exists($this, $db_field)) {
+                $properties[$db_field] = $this->$db_field;
+            }
+            return $properties;
+        }
+    }
+    // protected function prepare_sql_values()
+    // {
+    //     $sql_values = '';
+    //     for ($i = 2; $i <= count($this->properties()); $i++) {
+    //         ($i != count($this->properties())) ? $sql_values .= '?,' : $sql_values .= '?';
+    //     }
+    //     return $sql_values;
+    // }
 
+    // protected function get_properties_types()
+    // {
+    //     $obj_values =  array_values($this->properties());
+    //     $types = '';
+    //     for ($i = 1; $i < count($obj_values); $i++) {
+    //         (intval($obj_values[$i]) > 0) ? $types .= 'i' : $types .= 's';
+    //     }
+    //     return $types;
+    // }
 
-    public function create() 
+    // public function create()
+    // {
+    //     global $db;
+    //      $properties = $this->properties();
+    //      $keys = array_keys($properties);
+    //      unset($properties[$keys[0]]);
+
+    //     var_dump($properties);
+    //     $sql = "INSERT INTO " . self::$db_table . " (" . implode(', ', array_keys($properties)) . ") " . "VALUES (" . $this->prepare_sql_values() . ")";
+    //     var_dump($sql);
+
+    //     $parrams = [$this->username, $this->user_password, $this->user_first_name, $this->user_last_name,];
+    //     if ($stmt = $db->conn->prepare($sql)) {
+
+    //         $stmt->bind_param($this->get_properties_types(), ...$parrams);
+    //         $stmt->execute();
+    //         $this->user_id = $db->insert_id();
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    public function create()
     {
         global $db;
-        $sql = "INSERT INTO users (username, user_password, user_first_name, user_last_name) VALUES(?, ?, ?, ?)";
-        if($stmt = $db->conn->prepare($sql)) {
-            $stmt->bind_param("ssss", $this->username, $this->user_password, $this->user_first_name, $this->user_last_name);
-            $stmt->execute();
+        $properties = $this->properties();
+        $sql = "INSERT INTO " . self::$db_table . "(" . implode(',', array_keys($properties)) . ")" . " VALUES ('" . implode("','", array_values($properties)) . "')";
+        if ($db->query($sql)) {
             $this->user_id = $db->insert_id();
             return true;
         } else {
             return false;
         }
-
     }
 
     public function update()
     {
         global $db;
-        $sql = "UPDATE users SET username = ?, user_password = ?, user_first_name = ?, user_last_name = ? WHERE user_id = ?";
-        
-        $stmt = $db->conn->prepare($sql);
-            $stmt->bind_param("ssssi", $this->username, $this->user_password, $this->user_first_name, $this->user_last_name, $this->user_id);
-            $stmt->execute();
-            return ($db->conn->affected_rows == 1) ? true: false;
+        $sql = "UPDATE " . self::$db_table . " SET username = ?, user_password = ?, user_first_name = ?, user_last_name = ? WHERE user_id = ?";
 
+        $stmt = $db->conn->prepare($sql);
+        $stmt->bind_param("ssssi", $this->username, $this->user_password, $this->user_first_name, $this->user_last_name, $this->user_id);
+        $stmt->execute();
+        return ($db->conn->affected_rows == 1) ? true : false;
     }
     public function delete()
     {
         global $db;
-        $sql = "DELETE FROM users WHERE user_id = ? LIMIT 1";
+        $sql = "DELETE FROM " . self::$db_table . " WHERE user_id = ? LIMIT 1";
         $stmt = $db->conn->prepare($sql);
-            $stmt->bind_param("i", $this->user_id);
-            $stmt->execute();
-            return ($db->conn->affected_rows == 1) ? true: false;
-
+        $stmt->bind_param("i", $this->user_id);
+        $stmt->execute();
+        return ($db->conn->affected_rows == 1) ? true : false;
     }
 }
