af5acb9959cef15742b1fecd4099492a3f9488a4
Nikita <kent720p@gmail.com>
2022-06-14 02:18:58 +0200

    inherit user from parent class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,6 +1,6 @@
 <?php
 
-class User
+class User extends Db_object
 {
     protected static $db_table = "users";
     protected static $db_table_fields = array('username', 'user_password', 'user_first_name', 'user_last_name');
@@ -10,27 +10,6 @@ class User
     public $user_first_name;
     public $user_last_name;
 
-    public static function get_all_users()
-    {
-        return self::find_this_query("SELECT * FROM users");
-    }
-    public static function get_user_by_id($user_id)
-    {
-        $result_arr = self::find_this_query("SELECT * FROM users WHERE user_id = $user_id LIMIT 1");
-        return !empty($result_arr) ? array_shift($result_arr) : false;
-    }
-
-    public static function find_this_query($sql)
-    {
-        global $db;
-        $result_set = $db->query($sql)->fetch_all(MYSQLI_ASSOC);
-
-        $obj_arr = array();
-        foreach ($result_set as $row) {
-            $obj_arr[] = self::instatiation($row);
-        }
-        return $obj_arr;
-    }
 
     public static function verify_user($username, $password)
     {
@@ -43,49 +22,10 @@ class User
         return !empty($result_arr) ? array_shift($result_arr) : false;
     }
 
-    public static function instatiation($user_arr)
-    {
-        $user_obj = new self;
-        foreach ($user_arr as $attribute => $value) {
-            if ($user_obj->has_the_attribute($attribute)) {
-                $user_obj->$attribute = $value;
-            }
-        }
-        return $user_obj;
-    }
 
-    private function has_the_attribute($attr)
-    {
-        $object_properties = get_object_vars($this);
-        return array_key_exists($attr, $object_properties);
-    }
-    public function save()
-    {
-        return isset($this->user_id) ? $this->update() : $this->create();
-    }
 
-    protected function properties()
-    {
-        $properties = array();
-        foreach (self::$db_table_fields as $db_field) {
-            if (property_exists($this, $db_field)) {
-                $properties[$db_field] = $this->$db_field;
-            }
-        }
-        return $properties;
-    }
 
-    protected function clean_properties() 
-    {
-        global $db;
-        $clean_properties = array();
-        foreach($this->properties() as $key => $value){ 
-            if(!is_null($value)) {
-                $clean_properties[$key] = $db->escape_string($value);
-            }
-        }
-        return $clean_properties;
-    }
+
     // protected function prepare_sql_values()
     // {
     //     $sql_values = '';
@@ -127,34 +67,6 @@ class User
     //         return false;
     //     }
     // }
-    public function create()
-    {
-        global $db;
-        $properties = $this->clean_properties();
-        $sql = "INSERT INTO " . self::$db_table . "(" . implode(',', array_keys($properties)) . ")" . " VALUES ('" . implode("','", array_values($properties)) . "')";
-        if ($db->query($sql)) {
-            $this->user_id = $db->insert_id();
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public function update()
-    {
-
-        global $db;
-        $properties = $this->clean_properties();
-        var_dump($properties);
-        $properties_pairs = array();
-        foreach($properties as $key => $value) {
-            $properties_pairs[] = "{$key}='{$value}'";
-        }
-
-        $sql = "UPDATE " . self::$db_table . " SET " . implode(", ", $properties_pairs) . " WHERE user_id = $this->user_id";
-        var_dump($sql);
-        $db->query($sql);
-        return ($db->conn->affected_rows == 1) ? true : false;
-    }
 
     // public function update()
     // {
@@ -172,13 +84,4 @@ class User
     //     $stmt->execute();
     //     return ($db->conn->affected_rows == 1) ? true : false;
     // }
-    public function delete()
-    {
-        global $db;
-        $sql = "DELETE FROM " . self::$db_table . " WHERE user_id = ? LIMIT 1";
-        $stmt = $db->conn->prepare($sql);
-        $stmt->bind_param("i", $this->user_id);
-        $stmt->execute();
-        return ($db->conn->affected_rows == 1) ? true : false;
-    }
 }
