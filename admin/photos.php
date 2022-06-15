6f0d2e5e1f1da23d6153c0f7a8175687a27fd762
Nikita <kent720p@gmail.com>
2022-06-15 21:24:30 +0200

    changed photos apperance

--- admin/photos.php
+++ admin/photos.php
@@ -8,7 +8,7 @@ $photos = Photo::get_all();
         <div class="card m-3">
             <div class="card-body d-flex flex-row">
                 <div>
-                    <h5 class="card-title font-weight-bold mb-2"><?= $photo->photo_title ?></h5>
+                    <h5 class="card-title font-weight-bold mb-2"><?= $photo->title ?></h5>
                     <p class="card-text"><i class="far fa-clock pe-2"></i>07/24/2018</p>
                 </div>
             </div>
@@ -21,13 +21,13 @@ $photos = Photo::get_all();
             </div>
             <div class="card-body">
                 <p class="card-text collapse" id="collapseContent">
-                    <?= $photo->photo_description ?>
+                    <?= $photo->description ?>
                 </p>
-                <div class="d-flex justify-content-between">
-                    <a class="btn btn-link link-danger p-md-1 my-1" data-mdb-toggle="collapse" href="#collapseContent" role="button" aria-expanded="false" aria-controls="collapseContent">Read more</a>
+                <div class="d-flex justify-content-center">
+                    <a class="btn btn-link link-danger p-md-1 my-1" href="delete_photo.php?id=<?= $photo->id ?>" >Delete</a>
+                    <a class="btn btn-link link-primary p-md-1 my-1" href="edit_photo.php?id=<?= $photo->id ?>" >Edit</a>
+                    <a class="btn btn-link link-secondary p-md-1 my-1" >View</a>
                     <div>
-                        <i class="fas fa-share-alt text-muted p-md-1 my-1 me-2" data-mdb-toggle="tooltip" data-mdb-placement="top" title="Share this post"></i>
-                        <i class="fas fa-heart text-muted p-md-1 my-1 me-0" data-mdb-toggle="tooltip" data-mdb-placement="top" title="I like it"></i>
                     </div>
                 </div>
             </div>
