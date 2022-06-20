e13b0823639cdb822770b2cca804aaa8823accd4
Nikita <kent720p@gmail.com>
2022-06-21 01:42:37 +0200

    display comments count

--- admin/photos.php
+++ admin/photos.php
@@ -5,6 +5,7 @@ $photos = Photo::get_all();
 <div class="d-flex flex-wrap justify-content-center">
 
     <?php foreach ($photos as $photo) : ?>
+        <?php $comments = Comment::get_all_comments($photo->id) ?>
         <div class="card m-3">
             <div class="card-body d-flex flex-row">
                 <div>
@@ -27,7 +28,7 @@ $photos = Photo::get_all();
                     <a class="btn btn-link link-danger p-md-1 my-1" href="delete_photo.php?id=<?= $photo->id ?>" >Delete</a>
                     <a class="btn btn-link link-primary p-md-1 my-1" href="edit_photo.php?id=<?= $photo->id ?>" >Edit</a>
                     <a class="btn btn-link link-secondary p-md-1 my-1" href="/gallery/photo-details.php?photo=<?= $photo->id ?>" >View</a>
-                    <a class="btn btn-link link-info p-md-1 my-1" href="comments.php?photo=<?= $photo->id ?>" >Comments</a>
+                    <a class="btn btn-link link-info p-md-1 my-1" href="comments.php?photo=<?= $photo->id ?>" >Comments (<?= count($comments) ?>)</a>
                     <div>
                     </div>
                 </div>
