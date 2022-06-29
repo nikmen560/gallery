f9e2468c7ef5ca3e4a63790ec38587a67c26ef8d
Nikita <kent720p@gmail.com>
2022-06-29 03:43:51 +0200

    refactored index page

--- index.php
+++ index.php
@@ -3,14 +3,11 @@
 
 <?php
 $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
-$items_per_page = 2;
-$items_total_count = Photo::count_all();
-
-$paginate = new Paginate($page, $items_per_page, $items_total_count);
-$sql = "SELECT * FROM photos LIMIT {$items_per_page} OFFSET {$paginate->offset()}";
-$photos = Photo::find_by_query($sql);
+$items_per_page = 3;
+$items_total = Photo::count_all();
+$paginate = new Paginate($page, $items_per_page, $items_total);
+$photos = Photo::get_paginated_photos($items_per_page, $paginate);
 ?>
-
 
 <div class="container-fluid tm-container-content tm-mt-60">
     <div class="row mb-4">
@@ -19,7 +16,7 @@ $photos = Photo::find_by_query($sql);
         </h2>
         <div class="col-6 d-flex justify-content-end align-items-center">
             <form action="" class="tm-text-primary">
-                Page <input type="text" value="1" size="1" class="tm-input-paging tm-text-primary"> of 200
+                Page <input type="text" value="1" size="1" class="tm-input-paging tm-text-primary"> of <?= $paginate->page_total() ?>
             </form>
         </div>
     </div>
@@ -27,15 +24,15 @@ $photos = Photo::find_by_query($sql);
         <?php foreach ($photos as $photo) : ?>
             <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
                 <figure class="effect-ming tm-video-item">
-                    <img src="/gallery/admin/<?= $photo->picture_path() ?>" alt="Image" class="img-fluid">
+                    <img src="<?= $photo->picture_path() ?>" alt="Image" class="img-fluid">
                     <figcaption class="d-flex align-items-center justify-content-center">
                         <h2><?= $photo->title ?></h2>
                         <a href="photo-detail.php?photo=<?= $photo->id ?>">View more</a>
                     </figcaption>
                 </figure>
                 <div class="d-flex justify-content-between tm-text-gray">
-                    <span class="tm-text-gray-light">18 Oct 2020</span>
-                    <span>9,906 views</span>
+                    <span class="tm-text-gray-light"><?= $photo->date ?></span>
+                    <span><?= $photo->views ?> views</span>
                 </div>
             </div>
         <?php endforeach; ?>
@@ -45,7 +42,7 @@ $photos = Photo::find_by_query($sql);
             <?php if ($paginate->has_previous()) : ?>
                 <a href="/gallery/index.php?page=<?= $paginate->previous() ?>" class="btn btn-primary tm-btn-prev mb-2">Previous</a>
             <?php else : ?>
-                <a  class="btn btn-primary tm-btn-prev mb-2 disabled">Previous</a>
+                <a class="btn btn-primary tm-btn-prev mb-2 disabled">Previous</a>
             <?php endif; ?>
             <div class="tm-paging d-flex">
                 <?php
