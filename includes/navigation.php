2e0d7764256e6b05031cada2a1e94b1221b42e45
Nikita <kent720p@gmail.com>
2022-07-04 21:45:27 +0200

    log in link

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,14 +1,14 @@
 <?php require_once("head.php") ?>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/gallery/index.php">
-                <i class="fas fa-film mr-2"></i>
-                Gallery
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/gallery/index.php">
+            <i class="fas fa-film mr-2"></i>
+            Gallery
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link nav-link-1" aria-current="page" href="/gallery/index.php">Photos</a>
@@ -19,13 +19,17 @@
                 <li class="nav-item">
                     <a class="nav-link nav-link-3" href="contact.php">Contact</a>
                 </li>
-                <?php if(isset($session->id)): ?>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-4" href="/gallery/admin/index.php">Admin</a>
-                </li>
+                <?php if (isset($session->id)) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-4" href="/gallery/admin/index.php">Admin</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-5" href="/gallery/login.php">Login</a>
+                    </li>
                 <?php endif; ?>
             </ul>
-            </div>
         </div>
-    </nav>
-    <!-- TODO nav active link -->
+    </div>
+</nav>
+<!-- TODO nav active link -->
