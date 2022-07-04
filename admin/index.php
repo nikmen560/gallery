ba46b5e1939d50c670348a4114f47da53ebdf2d9
Nikita <kent720p@gmail.com>
2022-07-04 21:45:18 +0200

    changed number of photos per page

--- admin/index.php
+++ admin/index.php
@@ -1,50 +1,12 @@
 
         <?php include("includes/admin_navigation.php"); ?>
 
-        <div id="page-wrapper">
 
-            <div class="container-fluid">
+        <div id="piechart" style="width: 100%; height: 500px"></div>
 
-        <?php include("includes/admin_content.php"); ?>
-
-        <?php 
-
-        // $user_arr = ['username' => 'c', 'user_password' => 'c', 'user_first_name' => 'b', 'user_last_name' => 'pnx'];
-        // $user = User::get_user_by_id(2);
-        // $user = new User();
-        // $user->username = 'landfs';
-        // $user->user_password= 'dsafh';
-        // $user->user_last_name= 'sdjlfha';
-        // $user->save();
-        // var_dump(implode("','", array_keys($user_arr)));
-        // var_dump(array_values($user_arr));
-        // $photo = new Photo();
-        // $photo->photo_title = 'amigo';
-        // var_dump($photo->save());
-        // $users = Photo::get_all();
-        // foreach($users as $user)
-        // {
-        //     var_dump($user);
-        // }
-
-        $comments = Comment::get_all_comments(2);
-        
-        foreach($comments as $comment)
-        {
-            var_dump($comment);
-        }
+        <?php var_dump($session->count) ?>
+        <?php var_dump(Photo::count_all()) ?>
 
 
-
-        
-
-        // var_dump(intval('adsf'));
-        
-
-        ?>
-
-
-            </div>
-        </div>
 
   <?php include("includes/footer.php"); ?>
